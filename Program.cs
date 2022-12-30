b3a3b9d7c863d56b0c86917bc439b4ec69c65932
LittleFool <littlefool@fassiburg.de>
2022-12-30 23:47:06 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ed_journal_chat;
+
+Reader.RunWatcher();
+
+while(true)
+{
+    System.Threading.Thread.Sleep(1000);
+}
---
+++ Reader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ed_journal_chat
+{
+    internal class Reader
+    {
+        private static int ReadLinesCount = 0;
+        private static string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Saved Games\\Frontier Developments\\Elite Dangerous";
+        public static void RunWatcher()
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = path;
+            watcher.NotifyFilter = NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite;
+            watcher.Filter = "Journal.????-??-??T??????.??.log";
+            watcher.Changed += OnChanged;
+            watcher.EnableRaisingEvents = true;
+
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            var fs = new FileStream(e.FullPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using (var sr = new StreamReader(fs))
+            {
+                int totalLines = 0;
+                while (sr.ReadLine() != null) { totalLines++; }
+                int newLinesCount = totalLines - ReadLinesCount;
+                
+                fs.Position = 0;
+                sr.DiscardBufferedData();
+
+                for (int i = 0; i < ReadLinesCount; i++)
+                {
+                    sr.ReadLine();
+                }
+
+                string? line = sr.ReadLine();
+                while ( line != null )
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+
+                ReadLinesCount = totalLines;
+                //Console.WriteLine($"Changed: {e.FullPath}");
+                //Console.WriteLine($"new lines count: {newLinesCount}");
+            }
+        }
+    }
+}
